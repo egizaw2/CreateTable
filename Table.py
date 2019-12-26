b4dd8a7570ece666ce3dc818a287711bc0b3f1f7
egizaw2 <54922276+egizaw2@users.noreply.github.com>
2019-12-25 21:14:54 -0500

    Update Table.py

--- Table.py
+++ Table.py
@@ -1,2 +1,34 @@
 # CreateTable
 
+import boto3
+dynamodb = boto3.rescource('dynamodb')
+table = dynamodb.create_table(
+    TableName='Courses',
+    KeySchema=[
+        {
+            'AttributeName': 'HashKey',
+            'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'RangeKey',
+                'KeyType': 'RANGE'
+            }
+            
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'HashKey',
+                'AttributeType': 'N'
+            },
+            { 
+                'AttributeName': 'RangeKey',
+                'AttributeType': 'S'
+              },
+            
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+)
+print("Table status:", table.table_status)
